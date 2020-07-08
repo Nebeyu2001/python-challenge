abe9a7a5c4f21a455d486b990e24d5fa55ac41cf
Nebeyu Zewdie <nebeyu.zewdie@gmail.com>
2020-07-08 11:21:28 -0500

    final

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,11 +2,12 @@
 import os
 import csv
 
-# set path for file
+# set path for file to read and then to write our output
 csvpath = os.path.join("Resources","budget_data.csv")
 
 output_path = os.path.join("analysis", "bank_anlysis.txt")
 
+#initialize the starting value into 0
 Total_months = 0
 
 Net_total_amount= 0
@@ -15,10 +16,14 @@ change=0
 
 previous = 0
 
+# creat an empty list to fill later
 avg_change=[]
 
+# To find the greatest increase in profits,with the corresponding month, creating a list, with zero as first profit value
 inc=["",0]
 
+
+# To find the greatest decrease in losses,with the corresponding month,creating a list, with random high number to compare
 dec=["",99999]
 
 # open the CSV
@@ -37,51 +42,67 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     for row in csvreader:
 
+        # counting each month row by row in the CSV
         Total_months= Total_months + 1 
         
-        
+        # adding the total amount row by row, changing the string value of profits and losses(index[1])
         Net_total_amount= Net_total_amount + int(row[1])
 
+        # the change of profits/losses between the previous month and the curruent one 
         change= int(row[1])-previous
 
+        # put the changes in am empty list to calculate the average change later
         avg_change.append(change)
-
+        
+        # resets the previous value
         previous=int(row[1])
 
+        # The greaetst increase in profits means the "greatest change" , using indexes we get both month and profits 
+        # if the current change is higher than the previous, then it resets the value of change, till it finds the greatest in the entire period
         if change > inc[1]:
             inc[0] = row[0]
             inc[1]= change
             #print(inc)
 
+        
+        # the greatest decrease in losses means the "lowest change", using indexes gets both month and profits
+        # if the current change is lower than the previous,then it resets the value of change till it finds the lowest change in the list
         if change < dec[1]:
             dec[0] = row[0]
             dec[1]= change
             #print(dec)
+
+
+     # calculates the average amount of changes by adding all changes in the list divided by the lenght of list, excluding the first value which isnot a change from perious value,       
     avg_amount= sum(avg_change[1:])/(len(avg_change)-1)
+
+
+    # final outputs after looping through every row starting from the top excluding header
     
     print(f"Total_months: {Total_months}")
-    print(f"Net_total_amount: {Net_total_amount}")
-    #print(f"avg_change:{avg_change}")
-    print(f"avg_amount:{avg_amount}")
+    print(f"Net_total_amount: ${Net_total_amount}")
+    #print(f"avg_change:${avg_change}")
+    print(f"avg_amount:{avg_amount :.2f}")
     #print(max(avg_change))
     #print(min(avg_change))
-    print(inc)
-    print(dec)
+    print(f"Greatest increase in profits:{inc}")
+    print(f"Greatest decrease in profits:{dec}")
 
 
-
+# open the text file using write mode 
 with open(output_path, 'w') as text_file:
 
 
-    # Write the first row (column headers)
-    text_file.write(f"Total_months: {Total_months}\n")
-    text_file.write(f"Net_total_amount: {Net_total_amount}\n")
-    text_file.write(f"avg_amount:{avg_amount}\n")
-    #text_file.write(inc)
-    #text_file.write(dec)
-    # Write the second row
-    #csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
+    # Write the first row 
 
+    text_file.write(f"Financial Analysis \n")
+    text_file.write(f"--------------------------- \n")
+    text_file.write(f"Total_months: {Total_months}\n")
+    text_file.write(f"Net_total_amount: ${Net_total_amount}\n")
+    text_file.write(f"avg_amount:${avg_amount :.2f}\n")
+    text_file.write(f"Greatest increase in profits:{inc}\n")
+    text_file.write(f"Greatest decrease in profits:{dec}")
+    
 
       
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,14 +2,21 @@
 import os
 import csv
 
-# set path for file
+# set path for file to read and write
 csvpath = os.path.join("Resources","election_data.csv")
 
 output_path = os.path.join("analysis", "election_anlysis.txt")
 
+# initialize the total votes into zero before counting row by row
 total_votes= 0
 
+# start with an empty dictionary to fill
 candidates ={}
+
+max_vote_count=0
+
+
+# open CSV file for reading
 
 with open(csvpath) as csvfile:
 
@@ -25,20 +32,64 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     for row in csvreader:
 
+        # counting each vote row by row in CSV
         total_votes = total_votes + 1
 
+        # create a variable for the candidates name based on the index in CSV    
         name= row[2]
 
+
+        # adding the candidates name into an empty dictionary if itisnot there
         if name not in candidates:
 
              candidates[name]=1
-
+        # if the name is in the candidates list, start counting the vote
         else:
 
             candidates[name]=candidates[name] + 1
+            
+# after looping through whole csv row by row we get the total count of votes
+print(f"Total votes: {total_votes}")
 
-print(total_votes)
 
+# loop through the key and values in the dictionary 
 for candidate_name, vote_count in candidates.items():
-    print(f"{candidate_name}:{vote_count}")
+
+    # calculates the percentage of each candidates vote /first changing string into int
+    percentage= int(vote_count)/(total_votes)
+
+    # finding the greatest vote count in the dictionary and then associated key gives us winner candidate
+    if vote_count > max_vote_count:
+
+        max_vote_count = vote_count
+
+        winner = candidate_name
+
+        print(winner)
+
+    # prints each candidates name, the vote count and calculated percentage of vote count
+    print(f"{candidate_name}:{percentage:.3%} ({vote_count})")
+
+# open the text file using write mode 
+with open(output_path, 'w') as text_file:
+
+
+    # Write the first row 
+    text_file.write(f"Election Results\n")
+    text_file.write(f"------------------------------\n")
+    text_file.write(f"Total_votes: {total_votes}\n")
+    text_file.write(f"------------------------------\n")
+
+    for candidate_name, vote_count in candidates.items():
+
+        percentage= int(vote_count)/(total_votes)
+
+        text_file.write(f"{candidate_name}:{percentage:.3%} ({vote_count}) \n")
+
+    text_file.write(f"------------------------------\n")
+
+    text_file.write(f"Winner: {winner}")
+   
+    
+
         
